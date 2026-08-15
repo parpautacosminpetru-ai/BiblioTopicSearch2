17a089fc34f2a56e3f17abf3f93c8730a3501575
parpautacosminpetru-ai <parpautacosminpetru@gmail.com>
2026-08-15 17:29:33 +0300

    Fix professional NLI tokenizer probe

--- semantic-final/pro/nli_probe.py
+++ semantic-final/pro/nli_probe.py
@@ -1,28 +1,39 @@
 import os
 import numpy as np
 import onnx
-from transformers import AutoTokenizer
+from huggingface_hub import hf_hub_download, list_repo_files
+from transformers import AutoTokenizer, XLMRobertaTokenizer
 from onnxruntime_extensions import OrtPyFunction, gen_processing_models
 
 REPO = "onnx-community/multilingual-MiniLMv2-L6-mnli-xnli-ONNX"
 REV = "ca5daf3d11b6c4b3143b1f4602a2edfb64c3ad7e"
 OUT = "/tmp/nli_tokenizer.onnx"
 
-tokenizer = AutoTokenizer.from_pretrained(REPO, revision=REV)
-print("tokenizer", tokenizer.__class__.__name__, "max", tokenizer.model_max_length)
+files = list_repo_files(REPO, revision=REV)
+print("FILES", files)
+spm = hf_hub_download(REPO, filename="sentencepiece.bpe.model", revision=REV)
+print("SPM", spm, os.path.getsize(spm))
+
+fast = AutoTokenizer.from_pretrained(REPO, revision=REV)
+slow = XLMRobertaTokenizer(vocab_file=spm)
+print("fast", fast.__class__.__name__, "max", fast.model_max_length)
+print("slow", slow.__class__.__name__, "max", slow.model_max_length)
 
 premise = "Nu avea voie să părăsească orașul fără permisiune."
 hypothesis = "restricție de deplasare"
 packed = premise + " </s></s> " + hypothesis
 
-pair = tokenizer(premise, hypothesis, add_special_tokens=True, truncation=True, return_tensors="np")
-single = tokenizer(packed, add_special_tokens=True, truncation=True, return_tensors="np")
+pair = fast(premise, hypothesis, add_special_tokens=True, truncation=True, return_tensors="np")
+single = fast(packed, add_special_tokens=True, truncation=True, return_tensors="np")
+slow_single = slow(packed, add_special_tokens=True, truncation=True, return_tensors="np")
 print("pair ids", pair["input_ids"].tolist())
 print("packed ids", single["input_ids"].tolist())
+print("slow ids", slow_single["input_ids"].tolist())
 print("PAIR_PACK_EQ", np.array_equal(pair["input_ids"], single["input_ids"]))
+print("FAST_SLOW_EQ", np.array_equal(single["input_ids"], slow_single["input_ids"]))
 print("PAIR_MASK_EQ", np.array_equal(pair["attention_mask"], single["attention_mask"]))
 
-pre, _ = gen_processing_models(tokenizer, pre_kwargs={}, opset=17)
+pre, _ = gen_processing_models(slow, pre_kwargs={}, opset=17)
 onnx.save(pre, OUT)
 print("PRE_INPUTS", [(x.name, [d.dim_value or d.dim_param for d in x.type.tensor_type.shape.dim]) for x in pre.graph.input])
 print("PRE_OUTPUTS", [(x.name, x.type.tensor_type.elem_type, [d.dim_value or d.dim_param for d in x.type.tensor_type.shape.dim]) for x in pre.graph.output])
